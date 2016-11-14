e66199938279e355e6521650ad868cc832d00c1e
Zack <Zack@DESKTOP-GMAM2KP>
2016-11-14 01:23:35 -0500

    Implemented Army class

--- The Empire/src/empire/character/Army.java
+++ The Empire/src/empire/character/Army.java
@@ -1,6 +1,113 @@
 package empire.character;
 
-public class Army
-{
+public class Army {
+	
+	private int numArchers;
+	
+	private int numSoldiers;
+	
+	private int attack;
+	
+	private int defense;
+	
+	public Army() {
+		numArchers = 0;
+		numSoldiers = 0;
+		attack = 0;
+		defense = 0;
+	}
+	
+	public int getSize() {
+		return numArchers + numSoldiers;
+	}
+	
+	public int getNumArchers() {
+		return numArchers;
+	}
+	
+	public int getNumSoldiers() {
+		return numSoldiers;
+	}
+	
+	public int getDefense() {
+		return defense;
+	}
+	
+	public int getAttack() {
+		return attack;
+	}
+	
+	public void addArcher() {
+		numArchers++;
+		defense += Archer.defense;
+		attack += Archer.attack;
+	}
+	
+	public void addSoldier() {
+		numSoldiers++;
+		defense += Soldier.defense;
+		attack += Soldier.attack;
+	}
+	
+	public void loseArcher() {
+		numArchers--;
+		defense -= Archer.defense;
+		attack -= Archer.attack;
+	}
+	
+	public void loseSoldier() {
+		numSoldiers--;
+		defense -= Soldier.defense;
+		attack -= Soldier.attack;
+	}
+	
+	public void upgradeArchers() {
+		Archer.upgradeAttack();
+		Archer.upgradeDefense();
+		updateStats();
+	}
+	
+	public void upgradeSoldiers() {
+		Soldier.upgradeAttack();
+		Soldier.upgradeDefense();
+		updateStats();
+	}
+	
+	private void updateStats() {
+		defense = numArchers*Archer.defense + numSoldiers*Soldier.defense;
+		attack = numArchers*Archer.attack + numSoldiers*Soldier.attack;
+	}
+	
+	private static class Archer {
+		
+		private static int attack = 5;
+		
+		private static int defense = 2;
+		
+		private static void upgradeAttack() {
+			attack += 2;
+		}
+		
+		private static void upgradeDefense() {
+			defense++;
+		}
+		
+	}
+	
+	private static class Soldier {
+		
+		private static int attack = 2;
+		
+		private static int defense = 5;
+		
+		private static void upgradeAttack() {
+			attack++;
+		}
+		
+		private static void upgradeDefense() {
+			defense += 2;
+		}
+		
+	}
 
 }
