21bec2a77d6ef768a9ba32c74f895639de3b2ba3
Zack <Zack@DESKTOP-GMAM2KP>
2016-11-15 00:06:02 -0500

    Nothing much

--- The Empire/src/empire/character/Population.java
+++ The Empire/src/empire/character/Population.java
@@ -2,12 +2,13 @@ package empire.character;
 
 public class Population {
 	/**
-	 * need a base level of max population and a current level of max population that can be upgradeable
+	 * need a base level of max population and a current level of max population
+	 * that can be upgradeable
 	 */
-	private static final int STARTER_MAX_POPULATION = 50;	
-	
+	private static final int STARTER_MAX_POPULATION = 50;
+
 	private static int currentMaxPopulation;
-	
+
 	private static int numCitizens;
 
 	private static int numBlacksmiths;
@@ -55,75 +56,66 @@ public class Population {
 	}
 
 	public int collectRent() {
-		return Citizen.payRent()*getPopulation();
+		return Citizen.payRent() * getPopulation();
 	}
 
-	private static class Citizen 
-	{
+	private static class Citizen {
 
-		private static int payRent() 
-		{
+		private static int payRent() {
 			return 10;
 		}
-		
-		private static void createBlackSmith()
-		{
-			numCitizens --;
-			numBlacksmiths ++;
+
+		private static void createBlackSmith() {
+			numCitizens--;
+			numBlacksmiths++;
 		}
-		private static void createShopKeeper()
-		{
-			numCitizens --;
-			numShopKeepers ++;
+
+		private static void createShopKeeper() {
+			numCitizens--;
+			numShopKeepers++;
 		}
-		private static void createFarmer()
-		{
-			numCitizens --;
-			numFarmers ++;
+
+		private static void createFarmer() {
+			numCitizens--;
+			numFarmers++;
 		}
-		private static void createHunter()
-		{
-			numCitizens --;
-			numHunters ++;
+
+		private static void createHunter() {
+			numCitizens--;
+			numHunters++;
 		}
 
 	}
-	
+
 	/**
 	 * 
-	 * Each of the subclasses of citizen could have
-	 * some sort of "upgrade" path where they can offer
-	 * different services or end up paying more rent.
+	 * Each of the subclasses of citizen could have some sort of "upgrade" path
+	 * where they can offer different services or end up paying more rent.
 	 *
 	 */
-	
-	private static class Blacksmith extends Citizen 
-	{
-		private static int payRent() 
-		{
+
+	private static class Blacksmith extends Citizen {
+		private static int payRent() {
 			return 15;
 		}
 	}
-	private static class ShopKeeper extends Citizen 
-	{
-		private static int payRent() 
-		{
+
+	private static class ShopKeeper extends Citizen {
+		private static int payRent() {
 			return 20;
 		}
 	}
-	private static class Hunter extends Citizen 
-	{
-		private static int payRent() 
-		{
+
+	private static class Hunter extends Citizen {
+		private static int payRent() {
 			return 12;
 		}
 	}
-	private static class Farmer extends Citizen 
-	{
-		private static int payRent() 
-		{
+
+	private static class Farmer extends Citizen {
+		private static int payRent() {
 			return 5;
 		}
-	}
 
+	}
 }
